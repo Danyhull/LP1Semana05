4deffbc75584671c18ea65f92a8e11fed55eb8eb
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-21 22:47:33 +0000

    Exercicio 10 realizado sem erros

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,18 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = int.Parse(args[0]);
+            Enemy[] b = new Enemy [a];
+            for (int i=0; i<=(a-1); i++)
+            {
+                Console.WriteLine($"Nome do Inimigo {i+1}: ");
+                string c =Console.ReadLine();
+                b[i] = new Enemy(c);
+            }
+            for (int i = 0; i<=(a-1); i++)
+            {
+                Console.WriteLine($"{b[i].GetName()} {b[i].GetHealth()} {b[i].GetShield()}" );
+            }
         }
     }
 }
