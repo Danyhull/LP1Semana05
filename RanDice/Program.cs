d1d337559361c9e824da7d9f6f799f8182fb4a92
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-21 21:55:34 +0000

    Exercicio 5

---
+++ RanDice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n= int.Parse(args[0]);
+            int s= int.Parse(args[1]);
+            Random rand = new Random(s);
+            for(int i = 0;i<n;i++)
+            {
+                int DiceVal = rand.Next(1,7);
+                sum += DiceVal;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
