1c7b7dbd381a78541bfe54d6834302d7b074852f
Daniel Silva <danielhull.silva1@gmail.com>
2024-03-21 21:37:41 +0000

    Programa executado sem erros Exercicio2

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -16,9 +16,18 @@ namespace Chronos
 
             crono2.Start();
             Thread.Sleep(200);
-            
+
             crono1.Stop();
             crono2.Stop();
+
+            long a = crono1.ElapsedMilliseconds;
+            long b = crono2.ElapsedMilliseconds;
+
+            double sg1 = a/1000.0;
+            double sg2 = b/1000.0;
+
+            Console.WriteLine(sg1);
+            Console.WriteLine(sg2);
 
             
         }
